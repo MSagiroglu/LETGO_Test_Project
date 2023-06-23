805ee99c7dc073df833cd531f2badda88386c2e6
Mustafa SAĞIROĞLU <mustafasagiroglu0645@gmail.com>
2023-06-23 10:11:44 +0300

    last version

--- src/test/java/letgo_test_packages/utilities/TestBase.java
+++ src/test/java/letgo_test_packages/utilities/TestBase.java
@@ -1,7 +1,6 @@
-package tecproed.utilities;
+package letgo_test_packages.utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
