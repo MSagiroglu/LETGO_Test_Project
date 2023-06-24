c0bf7c1e0f53d4f8b0569c48cce18d69143cec54
Erol <122645226+erolisldk@users.noreply.github.com>
2023-06-24 17:57:51 +0300

    erol 24.06.2023

---
+++ src/test/java/letgo_test_packages/letgo_oto_plus/Erol_Tasks.java
@@ -0,0 +1,8 @@
+package letgo_test_packages.letgo_oto_plus;
+
+import letgo_test_packages.utilities.TestBase;
+
+public class Erol_Tasks extends TestBase {
+
+    // ben burada testlerimi kosacagim
+}
